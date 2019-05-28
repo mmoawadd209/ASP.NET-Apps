e28937cce7197bb86056e217936ab7a6fea25c16
mohamed moawad <48460844+mmoawadd209@users.noreply.github.com>
2019-05-28 04:39:26 +0200

    Applied Data Annotations and fixed bootstrap 3 vulnerability

--- Vidly/Vidly/Models/Customer.cs
+++ Vidly/Vidly/Models/Customer.cs
@@ -1,10 +1,14 @@
 ﻿
 
+using System.ComponentModel.DataAnnotations;
+
 namespace Vidly.Models
 {
     public class Customer
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255,ErrorMessage ="Maximum 255 charachters Allowed")]
         public string Name { get; set; }
         public bool IsSubscribedToNewsLetter { get; set; }
         public MembershipType MembershipType { get; set; }
--- Vidly/Vidly/Models/Movie.cs
+++ Vidly/Vidly/Models/Movie.cs
@@ -1,10 +1,13 @@
 ﻿
 
+using System.ComponentModel.DataAnnotations;
+
 namespace Vidly.Models
 {
     public class Movie
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
     }
 }
