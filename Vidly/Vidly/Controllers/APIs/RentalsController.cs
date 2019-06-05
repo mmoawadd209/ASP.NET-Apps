601cdfd8abc23fd86c19213b348d6ac77b2d89ef
mohamed moawad <48460844+mmoawadd209@users.noreply.github.com>
2019-06-05 20:26:26 +0200

    Added  NewRental Action to Rentals API

---
+++ Vidly/Vidly/Controllers/APIs/RentalsController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Web.Http;
+using Vidly.Models;
+using Vidly.View_Models;
+
+namespace Vidly.Controllers.APIs
+{
+    public class RentalsController : ApiController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RentalsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //api/Rentals
+        [HttpPost]
+        public IHttpActionResult NewRental( NewRentalViewModel newRenatl)
+        {
+            var customer = _context.Customers.Single(c => c.Id == newRenatl.CustomerId);
+
+            var movies = _context.Movies.Where(m => newRenatl.MoviesIds.Contains(m.Id));
+
+            foreach (var movie in movies)
+            {
+                if (movie.NumberInStock == 0)
+                    return BadRequest("Movie is not available.");
+                                                   
+                movie.NumberInStock--;
+                var rental = new Rental
+                {
+                    Customer = customer,
+                    Movie = movie,
+                    RentDate = DateTime.Now
+                };
+
+                _context.Rentals.Add(rental);
+            }
+
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    
+
+    }
+}
---
+++ Vidly/Vidly/View_Models/NewRentalViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.View_Models
+{
+    public class NewRentalViewModel
+    {
+        public int CustomerId { get; set; }
+        public List<int> MoviesIds { get; set; }
+    }
+}
